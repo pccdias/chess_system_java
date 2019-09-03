1e16860ada8548c1a9010bc64c10dcd502d0a56b
Paulo Cesar C Dias <pccdias@gmail.com>
2019-09-03 17:20:48 -0300

    ChessException and ChessPosition

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,10 +4,6 @@ public class Position {
 	private int row;
 	private int column;
 	
-	public void Position() {
-		
-	}
-
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
